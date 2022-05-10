c430de22d328532989820d5866edf3dd5297a57b
Gabriel Estacio <gestacio1310@gmail.com>
2022-05-10 15:59:33 -0400

    tercer

--- lib/card_image.dart
+++ lib/card_image.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'floating_action_button_green.dart';
 
 class CardImage extends StatelessWidget {
   String pathImage = "assets/img/ac1.jpg";
@@ -13,7 +14,7 @@ class CardImage extends StatelessWidget {
     final card = Container(
       height: 350.0,
       width: 250.0,
-      margin: EdgeInsets.only(top: 80.0, left: 80.0),
+      margin: EdgeInsets.only(top: 80.0, left: 20.0),
       decoration: BoxDecoration(
           image:
               DecorationImage(fit: BoxFit.cover, image: AssetImage(pathImage)),
@@ -27,6 +28,9 @@ class CardImage extends StatelessWidget {
           ]),
     );
 
-    return card;
+    return Stack(
+      alignment: const Alignment(0.9, 1.1),
+      children: [card, FloatingActionButtonGreen()],
+    );
   }
 }
---
+++ lib/floating_action_button_green.dart
@@ -0,0 +1,51 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class FloatingActionButtonGreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // ignore: todo
+    // TODO: implement createState
+
+    return _FloatingActionButtonGreen();
+  }
+}
+
+class _FloatingActionButtonGreen extends State<FloatingActionButtonGreen> {
+  // var icono = Icons.favorite_border;
+  bool _es_favorito = false;
+
+  void onPressedFav() {
+    setState(() {
+      _es_favorito = !_es_favorito;
+    });
+    // icono = Icons.favorite;
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text("Añadido a Favoritos")),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: todo
+    // TODO: implement build
+
+    final buttonFavorite = FloatingActionButton(
+      backgroundColor: Colors.green,
+      mini: true,
+      tooltip: "<3",
+      onPressed: onPressedFav,
+      child: Icon(_es_favorito ? Icons.favorite : Icons.favorite_border),
+    );
+
+    // if (es_favorito) {
+    //   return button_favorite;
+    // } else {
+    //   return button_favorite_border;
+    // }
+
+    return buttonFavorite;
+  }
+}
